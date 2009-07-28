1e7632dbfbcef5802c93f7da6661342e7bfd9fbd
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-07-28 00:31:04 +0000

    OK, Criação de Projeto com codigos exemplos para estudo e auxilio para inserir dados no banco de dados

---
+++ branches/TCC/CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/DAL/AcessoDados.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+namespace TelasDesenvolvedor.DAL
+{
+    class AcessoDados
+    {
+        #region Atributos
+
+        
+        private SqlDataAdapter dAdap;
+        private SqlCommand comando;
+
+        #endregion Atributos
+
+        #region Metodos
+
+        #region Execute Sql
+        /// <summary>
+        /// Executa a query
+        /// </summary>
+        /// <param name="query">query desejada</param>
+        /// <returns>DataTable populado com o retorno do banco de dados</returns>
+        protected DataTable ExecuteSql(string query)
+        {
+            DataTable dtRetorno = new DataTable();
+            try
+            {
+                dAdap = new SqlDataAdapter(query, ConectaBanco.Conexao);
+                dAdap.Fill(dtRetorno);
+                return dtRetorno;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                dtRetorno.Dispose();
+                dtRetorno = null;
+                dAdap.Dispose();
+                dAdap = null;
+            }
+        }
+        #endregion Execute Sql
+
+        protected bool InsereDados(string query)
+        {
+            try
+            {
+                comando = new SqlCommand(query, ConectaBanco.Conexao);
+                comando.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                comando = null;
+            }
+        }
+
+        #endregion Metodos
+    }
+}
---
+++ branches/TCC/CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/MODEL/mMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TelasDesenvolvedor.MODEL
+{
+    class mMenu
+    {
+        private int idMenu;
+        private string dscMenu;
+        private string endMenu;
+        private DateTime datAtl;
+        private bool flgAtivo;
+
+        public int IdMenu
+        {
+            get { return idMenu; }
+            set { idMenu = value; }
+        }
+
+        public string DscMenu
+        {
+            get { return dscMenu; }
+            set { dscMenu = value; }
+        }
+
+        public string EndMenu
+        {
+            get { return endMenu; }
+            set { endMenu = value; }
+        }
+
+        public DateTime DatAtl
+        {
+            get { return datAtl = DateTime.Now; }
+            set { datAtl = value; }
+        }
+        
+        public bool FlgAtivo
+        {
+            get { return flgAtivo = true; }
+            set { flgAtivo = value; }
+        }
+    }
+}
