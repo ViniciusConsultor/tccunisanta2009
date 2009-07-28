bbfcaa66bae0c3f8614fa2cd6a87e83004beec54
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-07-28 00:31:04 +0000

    OK, Criação de Projeto com codigos exemplos para estudo e auxilio para inserir dados no banco de dados

---
+++ CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/DAL/ConectaBanco.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TelasDesenvolvedor.DAL
+{
+    class ConectaBanco
+    {
+        private static SqlConnection conexao = new SqlConnection(@"Data Source=KAUE-PC\;Initial Catalog=Megatechdatabase;Integrated Security=True");
+
+        public static SqlConnection Conexao
+        {
+            get { return conexao; }
+        }
+
+        #region Conecta Banco
+        /// <summary>
+        /// Abre a conexão com o banco de dados.
+        /// </summary>
+        /// <returns>Caso true a conexão foi aberta com sucesso. Caso contrário false</returns>
+        public static bool ConectaBancoDados()
+        {
+            try
+            {
+                //TODO: Descobrir forma melhor de abrir uma conexão com o banco de dados.
+                //-----------------------------------------------------------------------
+                conexao.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion Conecta Banco
+
+        #region Desconecta Banco
+        /// <summary>
+        /// Desconecta do Banco de dados.
+        /// </summary>
+        /// <returns>Caso true que a desconecção foi feita com sucesso.</returns>
+        protected static bool DesconectaBanco()
+        {
+            try
+            {
+                conexao.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                conexao.Dispose();
+                conexao = null;
+            }
+        }
+        #endregion Desconecta Banco
+
+    }
+}
---
+++ CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/DAL/dMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TelasDesenvolvedor.MODEL;
+
+namespace TelasDesenvolvedor.DAL
+{
+    class dMenu : AcessoDados
+    {
+        public void CadastraMenu(mMenu model)
+        {
+            StringBuilder sql = new StringBuilder();
+            try
+            {
+                int flgAt;
+                if (model.FlgAtivo == true)
+                {
+                    flgAt = 1;
+                }
+                else
+                {
+                    flgAt = 0;
+                }
+                sql.Append(" INSERT INTO Menu ");
+                sql.Append(" (id_menu, dsc_menu, end_menu, dat_atl, flg_ativo) ");
+                sql.Append(" VALUES (" + model.IdMenu + ", '" + model.DscMenu + "', '" + model.EndMenu + "', '" + model.DatAtl.ToString("yyyy-MM-dd") + "', " + flgAt + ")");
+
+                base.InsereDados(sql.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sql = null;
+            }
+        }
+    }
+}
