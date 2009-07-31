9b9a92d6ca0501b132e02f26cb964f9da4bc7abf
superjbn <superjbn@gmail.com>
2009-07-31 14:45:09 +0000

    OK FRMCADCOLABORADOR NOK TODO O RESTO

---
+++ branches/TCC/CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/UI/frmCadColaborador.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace TelasDesenvolvedor.UI
+{
+    partial class frmCadColaborador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIdColaborador = new System.Windows.Forms.Label();
+            this.lblCdUsuario = new System.Windows.Forms.Label();
+            this.lblCdDepart = new System.Windows.Forms.Label();
+            this.lblNmColaborador = new System.Windows.Forms.Label();
+            this.lblDtNasc = new System.Windows.Forms.Label();
+            this.lblNmRua = new System.Windows.Forms.Label();
+            this.lblNumeroEnd = new System.Windows.Forms.Label();
+            this.lblComplemento = new System.Windows.Forms.Label();
+            this.lblCep = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.txtCdColab = new System.Windows.Forms.TextBox();
+            this.txtCdUsuario = new System.Windows.Forms.TextBox();
+            this.txtCdDepartamento = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.lblRg = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtRg = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.CbSexo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblIdColaborador
+            // 
+            this.lblIdColaborador.AutoSize = true;
+            this.lblIdColaborador.Location = new System.Drawing.Point(12, 13);
+            this.lblIdColaborador.Name = "lblIdColaborador";
+            this.lblIdColaborador.Size = new System.Drawing.Size(100, 13);
+            this.lblIdColaborador.TabIndex = 0;
+            this.lblIdColaborador.Text = "Codigo Colaborador";
+            // 
+            // lblCdUsuario
+            // 
+            this.lblCdUsuario.AutoSize = true;
+            this.lblCdUsuario.Location = new System.Drawing.Point(196, 13);
+            this.lblCdUsuario.Name = "lblCdUsuario";
+            this.lblCdUsuario.Size = new System.Drawing.Size(79, 13);
+            this.lblCdUsuario.TabIndex = 0;
+            this.lblCdUsuario.Text = "Codigo Usuario";
+            // 
+            // lblCdDepart
+            // 
+            this.lblCdDepart.AutoSize = true;
+            this.lblCdDepart.Location = new System.Drawing.Point(354, 15);
+            this.lblCdDepart.Name = "lblCdDepart";
+            this.lblCdDepart.Size = new System.Drawing.Size(110, 13);
+            this.lblCdDepart.TabIndex = 0;
+            this.lblCdDepart.Text = "Codigo Departamento";
+            // 
+            // lblNmColaborador
+            // 
+            this.lblNmColaborador.AutoSize = true;
+            this.lblNmColaborador.Location = new System.Drawing.Point(12, 41);
+            this.lblNmColaborador.Name = "lblNmColaborador";
+            this.lblNmColaborador.Size = new System.Drawing.Size(38, 13);
+            this.lblNmColaborador.TabIndex = 0;
+            this.lblNmColaborador.Text = "Nome ";
+            // 
+            // lblDtNasc
+            // 
+            this.lblDtNasc.AutoSize = true;
+            this.lblDtNasc.Location = new System.Drawing.Point(354, 41);
+            this.lblDtNasc.Name = "lblDtNasc";
+            this.lblDtNasc.Size = new System.Drawing.Size(89, 13);
+            this.lblDtNasc.TabIndex = 0;
+            this.lblDtNasc.Text = "Data Nascimento";
+            // 
+            // lblNmRua
+            // 
+            this.lblNmRua.AutoSize = true;
+            this.lblNmRua.Location = new System.Drawing.Point(12, 73);
+            this.lblNmRua.Name = "lblNmRua";
+            this.lblNmRua.Size = new System.Drawing.Size(27, 13);
+            this.lblNmRua.TabIndex = 0;
+            this.lblNmRua.Text = "Rua";
+            // 
+            // lblNumeroEnd
+            // 
+            this.lblNumeroEnd.AutoSize = true;
+            this.lblNumeroEnd.Location = new System.Drawing.Point(231, 73);
+            this.lblNumeroEnd.Name = "lblNumeroEnd";
+            this.lblNumeroEnd.Size = new System.Drawing.Size(44, 13);
+            this.lblNumeroEnd.TabIndex = 0;
+            this.lblNumeroEnd.Text = "Numero";
+            // 
+            // lblComplemento
+            // 
+            this.lblComplemento.AutoSize = true;
+            this.lblComplemento.Location = new System.Drawing.Point(354, 76);
+            this.lblComplemento.Name = "lblComplemento";
+            this.lblComplemento.Size = new System.Drawing.Size(71, 13);
+            this.lblComplemento.TabIndex = 0;
+            this.lblComplemento.Text = "Complemento";
+            // 
+            // lblCep
+            // 
+            this.lblCep.AutoSize = true;
+            this.lblCep.Location = new System.Drawing.Point(12, 111);
+            this.lblCep.Name = "lblCep";
+            this.lblCep.Size = new System.Drawing.Size(28, 13);
+            this.lblCep.TabIndex = 0;
+            this.lblCep.Text = "CEP";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(231, 111);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(34, 13);
+            this.lblBairro.TabIndex = 0;
+            this.lblBairro.Text = "Bairro";
+            // 
+            // txtCdColab
+            // 
+            this.txtCdColab.Location = new System.Drawing.Point(118, 10);
+            this.txtCdColab.Name = "txtCdColab";
+            this.txtCdColab.Size = new System.Drawing.Size(59, 20);
+            this.txtCdColab.TabIndex = 1;
+            // 
+            // txtCdUsuario
+            // 
+            this.txtCdUsuario.Location = new System.Drawing.Point(281, 10);
+            this.txtCdUsuario.Name = "txtCdUsuario";
+            this.txtCdUsuario.Size = new System.Drawing.Size(67, 20);
+            this.txtCdUsuario.TabIndex = 1;
+            // 
+            // txtCdDepartamento
+            // 
+            this.txtCdDepartamento.Location = new System.Drawing.Point(470, 12);
+            this.txtCdDepartamento.Name = "txtCdDepartamento";
+            this.txtCdDepartamento.Size = new System.Drawing.Size(79, 20);
+            this.txtCdDepartamento.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(65, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(283, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(449, 38);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(100, 20);
+            this.txtData.TabIndex = 1;
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(65, 70);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(155, 20);
+            this.txtRua.TabIndex = 1;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(281, 73);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(67, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(449, 73);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(100, 20);
+            this.txtComplemento.TabIndex = 1;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(65, 108);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(155, 20);
+            this.txtCEP.TabIndex = 1;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(281, 108);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(162, 20);
+            this.txtBairro.TabIndex = 1;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(15, 148);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(40, 13);
+            this.lblCidade.TabIndex = 2;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(65, 145);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(155, 20);
+            this.txtCidade.TabIndex = 3;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(231, 148);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(40, 13);
+            this.lblEstado.TabIndex = 2;
+            this.lblEstado.Text = "Estado";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(281, 145);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(162, 20);
+            this.txtEstado.TabIndex = 3;
+            // 
+            // lblRg
+            // 
+            this.lblRg.AutoSize = true;
+            this.lblRg.Location = new System.Drawing.Point(15, 185);
+            this.lblRg.Name = "lblRg";
+            this.lblRg.Size = new System.Drawing.Size(23, 13);
+            this.lblRg.TabIndex = 2;
+            this.lblRg.Text = "RG";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(231, 185);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(27, 13);
+            this.lblCpf.TabIndex = 2;
+            this.lblCpf.Text = "CPF";
+            // 
+            // txtRg
+            // 
+            this.txtRg.Location = new System.Drawing.Point(65, 182);
+            this.txtRg.Name = "txtRg";
+            this.txtRg.Size = new System.Drawing.Size(155, 20);
+            this.txtRg.TabIndex = 3;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(281, 182);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(162, 20);
+            this.txtCpf.TabIndex = 3;
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(15, 220);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 2;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // CbSexo
+            // 
+            this.CbSexo.FormattingEnabled = true;
+            this.CbSexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.CbSexo.Location = new System.Drawing.Point(65, 220);
+            this.CbSexo.Name = "CbSexo";
+            this.CbSexo.Size = new System.Drawing.Size(155, 21);
+            this.CbSexo.TabIndex = 4;
+            // 
+            // frmCadColaborador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 266);
+            this.Controls.Add(this.CbSexo);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtRg);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblRg);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCdDepartamento);
+            this.Controls.Add(this.txtCdUsuario);
+            this.Controls.Add(this.txtCdColab);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.lblCep);
+            this.Controls.Add(this.lblComplemento);
+            this.Controls.Add(this.lblNumeroEnd);
+            this.Controls.Add(this.lblNmRua);
+            this.Controls.Add(this.lblDtNasc);
+            this.Controls.Add(this.lblNmColaborador);
+            this.Controls.Add(this.lblCdDepart);
+            this.Controls.Add(this.lblCdUsuario);
+            this.Controls.Add(this.lblIdColaborador);
+            this.Name = "frmCadColaborador";
+            this.Text = "frmCadColaborador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIdColaborador;
+        private System.Windows.Forms.Label lblCdUsuario;
+        private System.Windows.Forms.Label lblCdDepart;
+        private System.Windows.Forms.Label lblNmColaborador;
+        private System.Windows.Forms.Label lblDtNasc;
+        private System.Windows.Forms.Label lblNmRua;
+        private System.Windows.Forms.Label lblNumeroEnd;
+        private System.Windows.Forms.Label lblComplemento;
+        private System.Windows.Forms.Label lblCep;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.TextBox txtCdColab;
+        private System.Windows.Forms.TextBox txtCdUsuario;
+        private System.Windows.Forms.TextBox txtCdDepartamento;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label lblRg;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtRg;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.ComboBox CbSexo;
+    }
+}
