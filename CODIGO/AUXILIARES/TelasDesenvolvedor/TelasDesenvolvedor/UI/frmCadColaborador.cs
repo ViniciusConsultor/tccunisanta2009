51c5ab1ba4f99c2712aa38183da4a860ae220c8d
superjbn <superjbn@gmail.com>
2009-08-01 14:47:05 +0000

    NOK frmCadColacorador.cs FALTA PEGAR ITEMS DOS TXTBOX E CBbOX

--- CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/UI/frmCadColaborador.cs
+++ CODIGO/AUXILIARES/TelasDesenvolvedor/TelasDesenvolvedor/UI/frmCadColaborador.cs
@@ -6,16 +6,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using TelasDesenvolvedor.MODEL;
 
 namespace TelasDesenvolvedor.UI
 {
     public partial class frmCadColaborador : Form
     {
+
+          #region Atributos
+        DAL.AcessoDados dal;
+        #endregion Atributos
+
         public frmCadColaborador()
         {
             InitializeComponent();
         }
 
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+            mCadColaborador modelColaborador = new mCadColaborador();
+            BUSINESS.rCadColaborador regraMenu = new BUSINESS.rCadColaborador();
+            try
+            {
+                modelColaborador = this.PegaDadosTela();
+                regraMenu.cadastraColaborador(modelColaborador);
+                MessageBox.Show("Cadastrado com sucesso!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                modelColaborador = null;
+            }
+        }
 
+        private void btnApaga_Click(object sender, EventArgs e)
+        {
+             //Varre todos os controles da tela.
+            //---------------------------------
+            foreach (Control controles in this.Controls)
+            {
+                //Verifica se é TextBox
+                //---------------------
+                if (controles.GetType().Equals(new TextBox().GetType()) == true)
+                {
+                    //Caso seja apaga seu conteudo
+                    //----------------------------
+                    controles.Text = string.Empty;
+                }
+            }
+        }
    }
 }
