8ee17c2cbd38e73d0482a0340aef0fc22cb6295b
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-09-16 02:42:41 +0000

    OK, Teste de Classe Abstrata

--- branches/TCC/BANCO DE DADOS/PROCEDURES/Procedure_Insert_Usuario.sql
+++ branches/TCC/BANCO DE DADOS/PROCEDURES/Procedure_Insert_Usuario.sql
@@ -1,5 +1,4 @@
 USE Megatechdatabase
-go
 
 CREATE PROCEDURE sp_insert_usuario
 @id_usu INT,
@@ -8,7 +7,34 @@ CREATE PROCEDURE sp_insert_usuario
 @obs_usu VARCHAR(100),
 @flg_ativo BIT
 AS
-INSERT INTO 
-Usuario (id_usu, login, senha, obs_usu, flg_ativo)
-VALUES
-(@id_usu, @login, @senha, @obs_usu, @flg_ativo)
+BEGIN TRY
+	IF (@login = '')
+	BEGIN
+		RAISERROR ('Mensagem', 16, 1)
+	END
+	ELSE
+	BEGIN
+		INSERT INTO 
+		Usuario (id_usu, login, senha, obs_usu, flg_ativo)
+		VALUES
+		(@id_usu, @login, @senha, @obs_usu, @flg_ativo)
+	END
+END TRY
+BEGIN CATCH
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+
+    SELECT 
+        @ErrorMessage = ERROR_MESSAGE(),
+        @ErrorSeverity = ERROR_SEVERITY(),
+        @ErrorState = ERROR_STATE();
+
+    -- Use RAISERROR inside the CATCH block to return error
+    -- information about the original error that caused
+    -- execution to jump to the CATCH block.
+    RAISERROR (@ErrorMessage, -- Message text.
+               @ErrorSeverity, -- Severity.
+               @ErrorState -- State.
+               );
+END CATCH;
