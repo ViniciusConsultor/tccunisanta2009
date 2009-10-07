1ef16437b99ecd1f6b6a33d323c3376bce1af63a
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-10-07 01:50:14 +0000

    OK Subindo Classes Julio, Telas Cadastro

---
+++ branches/TCC/CODIGO/TCC/TCC/UI/frmNumeroMotor.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TCC.UI
+{
+    partial class frmNumeroMotor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNumeroMotor));
+            this.txtCdNumeroMotor = new System.Windows.Forms.TextBox();
+            this.lblCdNumeroMotor = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblDsNumeroMotor = new System.Windows.Forms.Label();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnAceitar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCdNumeroMotor
+            // 
+            this.txtCdNumeroMotor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCdNumeroMotor.Location = new System.Drawing.Point(196, 40);
+            this.txtCdNumeroMotor.Name = "txtCdNumeroMotor";
+            this.txtCdNumeroMotor.Size = new System.Drawing.Size(100, 20);
+            this.txtCdNumeroMotor.TabIndex = 20;
+            // 
+            // lblCdNumeroMotor
+            // 
+            this.lblCdNumeroMotor.AutoSize = true;
+            this.lblCdNumeroMotor.Location = new System.Drawing.Point(53, 47);
+            this.lblCdNumeroMotor.Name = "lblCdNumeroMotor";
+            this.lblCdNumeroMotor.Size = new System.Drawing.Size(140, 13);
+            this.lblCdNumeroMotor.TabIndex = 19;
+            this.lblCdNumeroMotor.Text = "Codigo do Numero do Motor";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(56, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // lblDsNumeroMotor
+            // 
+            this.lblDsNumeroMotor.AutoSize = true;
+            this.lblDsNumeroMotor.Location = new System.Drawing.Point(53, 84);
+            this.lblDsNumeroMotor.Name = "lblDsNumeroMotor";
+            this.lblDsNumeroMotor.Size = new System.Drawing.Size(155, 13);
+            this.lblDsNumeroMotor.TabIndex = 21;
+            this.lblDsNumeroMotor.Text = "Descrição do Numero do Motor";
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(221, 138);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 25;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(140, 138);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 24;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceitar
+            // 
+            this.btnAceitar.Location = new System.Drawing.Point(59, 138);
+            this.btnAceitar.Name = "btnAceitar";
+            this.btnAceitar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceitar.TabIndex = 23;
+            this.btnAceitar.Text = "Confirmar";
+            this.btnAceitar.UseVisualStyleBackColor = true;
+            // 
+            // frmNumeroMotor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(324, 189);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnAceitar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblDsNumeroMotor);
+            this.Controls.Add(this.txtCdNumeroMotor);
+            this.Controls.Add(this.lblCdNumeroMotor);
+            this.Name = "frmNumeroMotor";
+            this.Text = "frmNumeroMotor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCdNumeroMotor;
+        private System.Windows.Forms.Label lblCdNumeroMotor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDsNumeroMotor;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnAceitar;
+    }
+}
---
+++ branches/TCC/CODIGO/TCC/TCC/UI/frmTipoMotor.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TCC.UI
+{
+    partial class frmTipoMotor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTipoMotor));
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnAceitar = new System.Windows.Forms.Button();
+            this.lblCdTipoMotor = new System.Windows.Forms.Label();
+            this.txtCdTipoMotor = new System.Windows.Forms.TextBox();
+            this.txtNmTipoMotor = new System.Windows.Forms.TextBox();
+            this.lblNmTipoMotor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(205, 113);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 16;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(124, 113);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 15;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceitar
+            // 
+            this.btnAceitar.Location = new System.Drawing.Point(43, 113);
+            this.btnAceitar.Name = "btnAceitar";
+            this.btnAceitar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceitar.TabIndex = 14;
+            this.btnAceitar.Text = "Confirmar";
+            this.btnAceitar.UseVisualStyleBackColor = true;
+            // 
+            // lblCdTipoMotor
+            // 
+            this.lblCdTipoMotor.AutoSize = true;
+            this.lblCdTipoMotor.Location = new System.Drawing.Point(40, 40);
+            this.lblCdTipoMotor.Name = "lblCdTipoMotor";
+            this.lblCdTipoMotor.Size = new System.Drawing.Size(124, 13);
+            this.lblCdTipoMotor.TabIndex = 17;
+            this.lblCdTipoMotor.Text = "Codigo do Tipo do Motor";
+            // 
+            // txtCdTipoMotor
+            // 
+            this.txtCdTipoMotor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCdTipoMotor.Location = new System.Drawing.Point(170, 33);
+            this.txtCdTipoMotor.Name = "txtCdTipoMotor";
+            this.txtCdTipoMotor.Size = new System.Drawing.Size(100, 20);
+            this.txtCdTipoMotor.TabIndex = 18;
+            // 
+            // txtNmTipoMotor
+            // 
+            this.txtNmTipoMotor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNmTipoMotor.Location = new System.Drawing.Point(170, 63);
+            this.txtNmTipoMotor.Name = "txtNmTipoMotor";
+            this.txtNmTipoMotor.Size = new System.Drawing.Size(100, 20);
+            this.txtNmTipoMotor.TabIndex = 20;
+            // 
+            // lblNmTipoMotor
+            // 
+            this.lblNmTipoMotor.AutoSize = true;
+            this.lblNmTipoMotor.Location = new System.Drawing.Point(45, 70);
+            this.lblNmTipoMotor.Name = "lblNmTipoMotor";
+            this.lblNmTipoMotor.Size = new System.Drawing.Size(119, 13);
+            this.lblNmTipoMotor.TabIndex = 19;
+            this.lblNmTipoMotor.Text = "Nome do Tipo do Motor";
+            // 
+            // frmTipoMotor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(292, 159);
+            this.Controls.Add(this.txtNmTipoMotor);
+            this.Controls.Add(this.lblNmTipoMotor);
+            this.Controls.Add(this.txtCdTipoMotor);
+            this.Controls.Add(this.lblCdTipoMotor);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnAceitar);
+            this.Name = "frmTipoMotor";
+            this.Text = "Tipo Motor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnAceitar;
+        private System.Windows.Forms.Label lblCdTipoMotor;
+        private System.Windows.Forms.TextBox txtCdTipoMotor;
+        private System.Windows.Forms.TextBox txtNmTipoMotor;
+        private System.Windows.Forms.Label lblNmTipoMotor;
+    }
+}
