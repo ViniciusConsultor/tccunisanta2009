b71f9646425fd77e7c564f4b994e1898e4aaac80
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-10-13 23:37:14 +0000

    OK, Alterando estrutura do repositorio

--- branches/TCC/BANCO DE DADOS/PROCEDURES/procidure_busca_maxEstoque.sql
+++
@@ -1,9 +0,0 @@
-USE Megatechdatabase
-IF OBJECT_ID('sp_busca_maxEstoque', 'P')IS NOT NULL
-	DROP PROCEDURE sp_busca_maxEstoque;
-GO
-
-CREATE PROCEDURE [dbo].[sp_busca_maxEstoque]
-AS
-SELECT MAX(id_estoque) as id_estoque
-FROM Estoque
