ff36b09590948c65387a60c915e13f60f2da043c
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-10-15 02:17:21 +0000

    NOK Faltou terminar desenvolvimento Usinagem, e aguardando troca de ids para peça para continuar o desenvolvimento

---
+++ branches/TCC/CODIGO/TCC/TCC/BUSINESS/rUsinagem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace TCC.BUSINESS
+{
+    class rUsinagem : ComandosSql
+    {
+        public int BuscaIdMaximoUsinagem()
+        {
+            DataTable dtId = new DataTable();
+            int id = 0;
+            try
+            {
+                dtId = base.BuscaIdMaximoTabelas("id_usinagem", "Usinagem");
+                if (dtId.Rows[0]["max"] != DBNull.Value)
+                {
+                    id = Convert.ToInt32(dtId.Rows[0]["max"]);
+                }
+                return ++id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dtId != null)
+                {
+                    dtId.Dispose();
+                    dtId = null;
+                }
+            }
+        }
+
+        public override void ValidarInsere(TCC.MODEL.ModelPai model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ValidarDeleta(TCC.MODEL.ModelPai model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ValidarAltera(TCC.MODEL.ModelPai model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- branches/TCC/CODIGO/TCC/TCC/UI/frmCadUsinagem.cs
+++ branches/TCC/CODIGO/TCC/TCC/UI/frmCadUsinagem.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using TCC.BUSINESS;
+using TCC.MODEL;
 
 namespace TCC.UI
 {
@@ -14,6 +15,50 @@ namespace TCC.UI
         public frmCadUsinagem()
         {
             InitializeComponent();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            this.LimparCampos();
+            this.BuscaIdMaximoUsinagem();
+        }
+
+        private void BuscaIdMaximoUsinagem()
+        {
+            rUsinagem regra = new rUsinagem();
+            try
+            {
+                this.txtCdUsinagem.Text = regra.BuscaIdMaximoUsinagem().ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                regra = null;
+            }
+        }
+
+        private void LimparCampos()
+        {
+            foreach (Control controle in this.Controls)
+            {
+                if (controle.GetType().Equals(new TextBox().GetType()) == true)
+                {
+                    controle.Text = string.Empty;
+                }
+            }
+        }
+
+        private void frmCadUsinagem_Load(object sender, EventArgs e)
+        {
+            this.BuscaIdMaximoUsinagem();
         }
     }
 }
