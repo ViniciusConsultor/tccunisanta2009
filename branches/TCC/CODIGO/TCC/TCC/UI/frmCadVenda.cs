cf720d06aa7ea7f93103fd8c94d53d823ce97a8a
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-10-16 02:05:36 +0000

    OK, Implementando classe pai para os forms facilitando sua implementação.

--- branches/TCC/CODIGO/TCC/TCC/UI/frmCadVenda.cs
+++ branches/TCC/CODIGO/TCC/TCC/UI/frmCadVenda.cs
@@ -9,12 +9,32 @@ using System.Windows.Forms;
 
 namespace TCC.UI
 {
-    public partial class frmCadVenda : Form
+    public partial class frmCadVenda : FormPai
     {
         public frmCadVenda()
         {
             InitializeComponent();
         }
 
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpa_Click(object sender, EventArgs e)
+        {
+            base.LimpaDadosTela(this);
+        }
+
+        private void btnVolta_Click(object sender, EventArgs e)
+        {
+            base.FechaTela(this);
+        }
+
+
+        public override void BuscaIdMaximo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
