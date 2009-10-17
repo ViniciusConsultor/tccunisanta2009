3ffd1399d95e9e68f79074f182ba6d350074468f
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-10-17 20:54:42 +0000

    OK Procedures Busca, Com conflito Resolver

--- BANCO DE DADOS/PROCEDURES/SELECT/Procedure_Busca_Motor.sql
+++ BANCO DE DADOS/PROCEDURES/SELECT/Procedure_Busca_Motor.sql
@@ -5,4 +5,8 @@ IF OBJECT_ID('sp_busca_motor','P')IS NOT NULL
 GO
 CREATE PROCEDURE sp_busca_motor
 AS
+<<<<<<< .mine
+SELECT id_motor, dsc_motor 'Motor' FROM motor
+=======
 SELECT id_motor_compra, dsc_motor_compra as 'Descrição do motor' fROM motor
+>>>>>>> .r88
--- BANCO DE DADOS/PROCEDURES/SELECT/Procedure_Busca_UsuarioParam.sql
+++ BANCO DE DADOS/PROCEDURES/SELECT/Procedure_Busca_UsuarioParam.sql
@@ -7,6 +7,11 @@
  CREATE PROCEDURE sp_busca_usuario_param
  @login VARCHAR(20)
  AS
+<<<<<<< .mine
+ SELECT id_usu, log_usu 'Usuário' FROM Usuario
+ WHERE log_usu LIKE @login + '%'
+=======
  SELECT id_usu, login AS 'Login usuario' FROM Usuario
  WHERE login LIKE @login + '%'
+>>>>>>> .r88
 go
