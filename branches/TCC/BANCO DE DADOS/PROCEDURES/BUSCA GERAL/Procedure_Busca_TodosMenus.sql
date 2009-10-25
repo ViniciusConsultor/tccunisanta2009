420c016cc6d7ee68a78855aac134497b8d67604b
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-10-25 00:00:10 +0000

    OK, Validação data.

--- branches/TCC/BANCO DE DADOS/PROCEDURES/BUSCA GERAL/Procedure_Busca_TodosMenus.sql
+++ branches/TCC/BANCO DE DADOS/PROCEDURES/BUSCA GERAL/Procedure_Busca_TodosMenus.sql
@@ -6,5 +6,11 @@
  
  CREATE PROCEDURE sp_busca_todosMenus
   AS
- SELECT id_menu, dsc_menu 'Menu' FROM Menu
+ SELECT m.id_menu, m.dsc_menu 'Menu' 
+ FROM Menu m
+ INNER JOIN Submenu sm
+ ON m.id_menu = sm.id_menu_pai
+ WHERE 
+ m.id_menu NOT IN (SELECT id_menu_filho FROM submenu)
+ GROUP BY m.id_menu, m.dsc_menu
 go
---
+++ branches/TCC/CODIGO/TCC/TCC/BUSINESS/UTIL/Validacoes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCC.BUSINESS.UTIL
+{
+    public static class Validacoes
+    {
+        const int ANOMINIMO = 1753;
+
+        public static void ValidaData(string DataSemFormatacao)
+        {
+            string[] dataString = DataSemFormatacao.Split('/');
+            int[] data = new int[3];
+            int[] mes = new int[13];
+            for (int cont = 0; cont < dataString.Length; cont++)
+            {
+                data[cont] = Convert.ToInt32(dataString[cont]);
+            }
+            mes[1] = 31;
+            if (Validacoes.VerificaAnoBixesto(data[2]) == true)
+            {
+                mes[2] = 28;
+            }
+            else
+            {
+                mes[2] = 29;
+            }
+            mes[3] = 31;
+            mes[4] = 30;
+            mes[5] = 31;
+            mes[6] = 30;
+            mes[7] = 31;
+            mes[8] = 31;
+            mes[9] = 30;
+            mes[10] = 31;
+            mes[11] = 30;
+            mes[12] = 31;
+
+            if (data[2] < ANOMINIMO)
+            {
+                throw new Exceptions.Validacoes.DataInvalidaException(TCC.BUSINESS.Exceptions.Validacoes.TipoErroData.ano, data);
+            }
+            else if (data[1] > mes.Length || data[1] < 1)
+            {
+                throw new Exceptions.Validacoes.DataInvalidaException(TCC.BUSINESS.Exceptions.Validacoes.TipoErroData.mes, data);
+            }
+            else if (data[0] > mes[data[1]] || data[0] < 1)
+            {
+                throw new Exceptions.Validacoes.DataInvalidaException(TCC.BUSINESS.Exceptions.Validacoes.TipoErroData.dia, data);
+            }
+        }
+
+        private static bool VerificaAnoBixesto(int ano)
+        {
+            bool retorno = false;
+            if (ano % 400 == 0)
+            {
+                retorno = true;
+            }
+            else if (ano % 100 == 0 && ano % 400 != 0)
+            {
+                retorno = false;
+            }
+            else if (ano % 4 == 0 && ano % 100 != 0)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
