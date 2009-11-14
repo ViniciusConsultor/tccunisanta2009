851f330ffa8026f793146b6309137065693c3b79
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-11-14 17:09:52 +0000

    NOK, Tela Resumo Ordem Produção Não terminado. Procs

--- BANCO DE DADOS/PROCEDURES/PROCEDURES GERAL/Procedure_Busca_TreeFamiliaMotor.sql
+++ BANCO DE DADOS/PROCEDURES/PROCEDURES GERAL/Procedure_Busca_TreeFamiliaMotor.sql
@@ -1,15 +1,18 @@
 USE Megatechdatabase
 GO
 
-IF OBJECT_ID('sp_busca_tree_familiaMotor','p') IS NOT NULL
-		DROP PROCEDURE sp_busca_tree_familiaMotor;
+IF OBJECT_ID('sp_busca_tree_familiamotor','p') IS NOT NULL
+		DROP PROCEDURE sp_busca_tree_familiamotor;
 GO
 
-CREATE PROCEDURE sp_busca_tree_familiaMotor
+CREATE PROCEDURE sp_busca_tree_familiamotor
 @id_fam_motor INT
 AS
-SELECT fm.id_fam_motor, fm.id_fam_motor_real, kf.id_kit
+SELECT kf.id_kit, fm.id_fam_motor, fm.id_num_motor, fm.dsc_fam_motor, fm.flg_ativo, fm.id_motor, fm.id_estoque, fm.id_fam_motor_real, fm.id_tipo_motor
 FROM Familiamotor fm
 INNER JOIN Kitfamilia kf
 ON fm.id_fam_motor = kf.id_fam_motor
 WHERE fm.id_fam_motor = @id_fam_motor
+AND fm.flg_ativo = 1
+
+-- exec sp_busca_tree_familiamotor 1
