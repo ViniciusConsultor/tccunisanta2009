f897ac85a7234ee86834df9b308eede7db99dfc9
super.nice.guy87 <super.nice.guy87@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-11-14 16:34:26 +0000

    OK Inserts inicias

--- branches/TCC/BANCO DE DADOS/PROCEDURES/SELECT/Procedure_Select_Estoque.sql
+++ branches/TCC/BANCO DE DADOS/PROCEDURES/SELECT/Procedure_Select_Estoque.sql
@@ -8,7 +8,7 @@ GO
 CREATE PROCEDURE sp_select_estoque
 @id_estoq INT
 AS
-SELECT id_estoq, dsc_estoq, dat_alt, id_depto, flg_negativo, flg_ativo
+SELECT id_estoq, dsc_estoq, dat_alt, id_depto, flag_negativo, flg_ativo
 FROM Estoque
 WHERE id_estoq = @id_estoq
 AND flg_ativo = 1
