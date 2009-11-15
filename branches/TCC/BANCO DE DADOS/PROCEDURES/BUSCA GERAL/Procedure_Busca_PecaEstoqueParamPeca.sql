4f2e653c2eb6aa897ecc42ab541b056668dca243
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-11-15 22:10:55 +0000

    NOK PerfilMenu

---
+++ branches/TCC/BANCO DE DADOS/PROCEDURES/BUSCA GERAL/Procedure_Busca_PecaEstoqueParamPeca.sql
@@ -0,0 +1,13 @@
+USE Megatechdatabase
+GO
+IF OBJECT_ID('sp_busca_pecaestoqueparampeca','P')IS NOT NULL	
+		DROP PROCEDURE sp_busca_pecaestoqueparampeca;
+GO
+CREATE PROCEDURE sp_busca_pecaestoqueparampeca
+@id_peca INT
+AS
+SELECT id_peca, id_estoq, qtd_peca as 'Quantidade da peça' FROM pecaestoque
+WHERE flg_ativo = 1
+	AND id_peca = @id_peca
+
+-- exec sp_busca_pecaestoqueparampeca 1
