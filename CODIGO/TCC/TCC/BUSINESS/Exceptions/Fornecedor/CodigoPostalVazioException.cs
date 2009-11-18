58ddec7f2f9d10399e3196122fd50d5eb1c7f839
super.nice.guy87 <super.nice.guy87@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-11-18 20:58:23 +0000

    OK Exceptions Cliente e Fornecedor

---
+++ CODIGO/TCC/TCC/BUSINESS/Exceptions/Fornecedor/CodigoPostalVazioException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCC.BUSINESS.Exceptions.Fornecedor
+{
+    public class CodigoPostalVazioException : Exception
+    {
+
+    }
+}
