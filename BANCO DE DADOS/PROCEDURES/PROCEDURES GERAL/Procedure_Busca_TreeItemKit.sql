11be75ba5c345d90e3b434102ca95f579b7f9dd8
kauefabioo <kauefabioo@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-11-20 17:42:13 +0000

    NOK, Manutenção classes

--- BANCO DE DADOS/PROCEDURES/PROCEDURES GERAL/Procedure_Busca_TreeItemKit.sql
+++ BANCO DE DADOS/PROCEDURES/PROCEDURES GERAL/Procedure_Busca_TreeItemKit.sql
@@ -8,9 +8,11 @@ GO
 CREATE PROCEDURE sp_busca_tree_itemkit
 @id_kit INT
 AS
-SELECT id_item, id_kit, dat_alt, qtd_item, flg_ativo
-FROM itemkit
+SELECT ik.id_item, ik.id_kit, ik.dat_alt, ik.qtd_item, ik.flg_ativo, i.id_item_real
+FROM itemkit ik
+INNER JOIN Item i
+ON ik.id_item = i.id_item
 WHERE id_kit = @id_kit
-AND flg_ativo = 1
+AND ik.flg_ativo = 1
 
--- exec sp_busca_tree_itemkit 1
+-- exec sp_busca_tree_itemkit 3
