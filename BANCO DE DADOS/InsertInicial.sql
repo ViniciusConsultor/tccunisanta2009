2c99be32bef5f51e6bf8314091f9bf355661543b
superjbn <superjbn@gmail.com>
2009-11-20 18:51:38 +0000

    insert inicial arrumado

--- BANCO DE DADOS/InsertInicial.sql
+++ BANCO DE DADOS/InsertInicial.sql
@@ -1,14 +1,6 @@
 USE Megatechdatabase
 go
 
---INSERT TABELA DEPARTAMENTO
---INSERT INTO Departamento (id_depto, dsc_depto, dat_atl, nom, flg_ativo) VALUES (1, 'DEPARTAMENTO 1', GETDATE(), 'Depto 01', 1);
---INSERT INTO Departamento (id_depto, dsc_depto, dat_atl, nom, flg_ativo) VALUES (2, 'DEPARTAMENTO 2', GETDATE(), 'Depto 02', 1);
---INSERT INTO Departamento (id_depto, dsc_depto, dat_atl, nom, flg_ativo) VALUES (3, 'DEPARTAMENTO 3', GETDATE(), 'Depto 03', 1);
-
---INSERT TABELA TIPO_DE_USUÁRIO
---INSERT INTO Tipousuario (id_tipo_usuario, dsc_tipo_usuario) VALUES (1, 'ADM');
-
 --INSERT TABELA ESTADO
 INSERT INTO Estado (slg_est, nom_est) VALUES ('AC', 'ACRE');
 INSERT INTO Estado (slg_est, nom_est) VALUES ('AL', 'ALAGOAS');
@@ -39,7 +31,7 @@ INSERT INTO Estado (slg_est, nom_est) VALUES ('SE', 'SERGIPE');
 INSERT INTO Estado (slg_est, nom_est) VALUES ('TO', 'TOCANTINS');
 
 --INSERT MENU
---INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Buscas', NULL, GETDATE(), 1);
+
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Compra', NULL, GETDATE(), 1);
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Venda', NULL, GETDATE(), 1);
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Estoque', NULL, GETDATE(), 1);
@@ -66,6 +58,7 @@ INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Realizar Venda', 
 --INSERT ESTOQUE 4
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Cadastrar estoques', 'frmCadEstoque', GETDATE(), 1);
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Movimentação de Peças', 'frmCadPecaEstoque', GETDATE(), 1);
+INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Buscas', NULL, GETDATE(), 1);
 
 --INSERT PRODUÇÃO 5
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Peças', NULL, GETDATE(), 1);
@@ -87,65 +80,63 @@ INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Cadastrar Perfis'
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Cadastrar Funcionário ', 'frmCadColaborador', GETDATE(), 1);
 INSERT INTO Menu (dsc_menu, ende, dat_atl, flg_ativo) VALUES ('Relacionar Menus ao Perfil', 'frmPerfilMenu', GETDATE(), 1);
 
---BUSCA
---INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 1);--Estoque/Busca
---INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (1, 7);--BuscaPeça
---INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (1, 8);--Busca Item
---INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (1, 9);--kit
---INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (1, 10);--Busca Familia motor
+
 
 --COMPRA
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (2, 11);--fornecedor
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (11, 12);--fornecedor/cadforn..
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (11, 13);--fornecedor--peça/forn
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (2, 14);--forneceodor/ Realiza compra
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (1, 10);--fornecedor
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (10, 11);--fornecedor/cadforn..
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (10, 12);--fornecedor--peça/forn
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (1, 13);--forneceodor/ Realiza compra
 
 --VENDA
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (3, 15);--cadVEnda
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (3, 16);--CadCliente
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (3, 17);--RealizaVenda
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (2, 14);--cadVEnda
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (2, 15);--CadCliente
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (2, 16);--RealizaVenda
 
 --ESTOQUE
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 18);--cadastro estoque
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 19);--Movimenta Peça
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (3, 17);--cadastro estoque
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (3, 18);--Movimenta Peça
+--BUSCA
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (3, 19);--Estoque/Busca
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (19, 20);--BuscaPeça
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (19, 21);--Busca Item
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (19, 22);--kit
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (19, 23);--Busca Familia motor
 
 --PRODUÇÃO
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (20, 21);--peças/tipo peca
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (20, 22);--peças/informar usinagem
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (20, 23);--peças/cadastro peça
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (5, 20);--produção/peça
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (5, 24);--cadastra item peça
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (5, 25);--cadastra kit
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (5, 26);--produção/motor
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (6, 21);--peças/tipo peca
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (6, 22);--peças/informar usinagem
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (6, 23);--peças/cadastro peça
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 20);--produção/peça
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 24);--cadastra item peça
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 25);--cadastra kit
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (4, 26);--produção/motor
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (26, 27);--motores/tipo motor
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (26, 28);--motores/numero motor
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (26, 29);--motores/cadastro motor
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (26, 30);--motores/familha motor
 
 --CADASTROS
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (6, 31);--cad departamento
-INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (6, 32);--cadastro/usuario
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (5, 31);--cad departamento
+INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (5, 32);--cadastro/usuario
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (32, 33);--usuario/cad perfil
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (32, 34);--usuario/cad funcionario
 INSERT INTO Submenu (id_menu_pai, id_menu_filho) VALUES (32, 35);--usuario/relacionaperfilmenu
-
 
 --INSERT TABELA PERFIL
 INSERT INTO Perfil (id_perfil, dsc_perfil, dat_atl, flg_ativo) VALUES (1, 'DEFAULT', GETDATE(), 1);
 
 --INSERT TABELA PERFILMENU
---INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 1, GETDATE(), 1);
+INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 1, GETDATE(), 1);
 INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 2, GETDATE(), 1);
 INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 3, GETDATE(), 1);
 INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 4, GETDATE(), 1);
 INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 5, GETDATE(), 1);
-INSERT INTO Perfilmenu (id_perfil, id_menu, dat_alt, flg_ativo) VALUES (1, 6, GETDATE(), 1);
+
 
 --INSERT TABELA TIPOPRODUTO
 INSERT INTO Tipoproduto (nom, flg_ativo, dat_alt) VALUES ('Motor', 1, GETDATE());
 INSERT INTO Tipoproduto (nom, flg_ativo, dat_alt) VALUES ('Kit', 1, GETDATE());
-
-
 
 --INSERT TABELA USUARIO senha: 123
 INSERT INTO Usuario (id_usu, log_usu, senha, obs_usu, flg_ativo, id_perfil) VALUES (1, 'kaue', 'MTIz', 'SEI LA', 1, 1);
@@ -195,8 +186,3 @@ INSERT INTO Item (id_item, nom, flg_ativo, id_item_real, dat_alt)VALUES ( 3,'ite
 INSERT INTO Colaborador(id_colab, nom, dat_nasc, rua, nro_ende, compl, cep, bairr, ddd, cid, tel, rg, cpf, sex, mail, dat_atl, flg_ativo, id_usu, id_depto, slg_est)VALUES (1, 'Andre',1982-05-04, 'Carvalho de Mendonça', 478, 'Casa', 11070102, 'Vila Belmiro', 13, 'Santos', 32398027, 441014227, 37006765811, 'M', 'andre@mail.com', GETDATE(), 1, 1,3,'SP');
 
 
---INSERT TABELA COLABORADOR
---INSERT INTO Colaborador (id_colab, nom, dat_nasc, rua, nro_ende, compl, cep, bairr, ddd, cid, tel, rg, cpf, sex, mail, dat_atl, flg_ativo, id_usu, id_depto, slg_est) VALUES (1, 'Kaue', '1988-06-02', 'Av. Ana Costa', 516, 'Sem Complemento', '11060002', 'Gonzaga', 13, 'Santos', '38828678', '321452015120321', '145266987125012', 'M', 'kauepaulo@hotmail.com', GETDATE(), 1, 1, 1, 'SP');
-
---INSERT TABELA CLIENTE 
---INSERT INTO cliente (id_cli, nom,tel,rua,nro_ende,compl,cep,bairr,cid,dat_atl,flg_ativo,slg_est,mail,ddd,cpf,cnpj, nom_pais) VALUES (1, 'julio','32322970','baralho',666,'castelo','11111111','Iron Forge','Storm Wind',GETDATE(),1,'SP','wow@mail.com',99,'37006765811',null, 'Brasil');
