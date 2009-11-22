550f1f61d89fa0a3fd3a9b4fed31896c46ea72fa
superjbn <superjbn@gmail.com>
2009-11-22 17:32:41 +0000

    ok julio peripecias de julio

---
+++ branches/TCC/CODIGO/TCC/TCC/UI/CADASTRO/frmMotorEstoque.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TCC.UI.CADASTRO
+{
+    public partial class frmMotorEstoque : Form
+    {
+        public frmMotorEstoque()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionaPeca_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
