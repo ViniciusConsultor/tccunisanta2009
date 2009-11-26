798ea513253dec370765686cf146205f24bd6f48
super.nice.guy87 <super.nice.guy87@29f01b9a-68e2-11de-9b35-9190732fe600>
2009-11-26 19:43:56 +0000

    NOK frmMotorEstoque NOK frmChegadaInsumos(o que está comentado ainda tem q fazer...ou seja..quase tudo)

--- CODIGO/TCC/TCC/UI/CADASTRO/frmChegadaInsumos.cs
+++ CODIGO/TCC/TCC/UI/CADASTRO/frmChegadaInsumos.cs
@@ -3,17 +3,310 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using TCC.BUSINESS;
+using TCC.MODEL;
 
-namespace TCC.UI.CADASTRO
+namespace TCC.UI
 {
-    public partial class frmChegadaInsumos : Form
+    public partial class frmChegadaInsumos : FormPai
     {
+        #region Atributos
+
+        mOrdemServico _modelOrdemServico;
+        mPeca _modelPeca;
+        mPecaEstoque _modelPecaEstoque;
+        mPecaFornecedor _modelPecaFornecedor;
+        mMotor _modelMotor;
+        mMotorEstoque _modelMotorEstoque;
+        mMotorFornecedor _modelMotorFornecedor;
+        mEstoque _modelEstoque;
+        mCompra _modelCompra;
+
+        #endregion Atributos
+
+        #region Construtor
+ 
         public frmChegadaInsumos()
         {
             InitializeComponent();
         }
+
+        #endregion Construtor
+
+        #region Eventos
+
+        #region btnAceitar Click
+        private void btnAceitar_Click(object sender, EventArgs e)
+        {
+            this.Insere();
+        }
+        #endregion btnAceitar Click
+
+        #region btnLimpar Click
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            base.LimpaDadosTela(this);
+
+            _modelMotorEstoque = null;
+            _modelMotor = null;
+
+            //this.PopulaGrid();
+        }
+        #endregion btnLimpar Click
+
+        #region btnBuscaAlteracaoDelecao Click
+        private void btnBuscaAlteracaoDelecao_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion btnBuscaAlteracaoDelecao Click
+
+        #region btnVoltar Click
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            base.FechaTela(this);
+        }
+        #endregion btnVoltar Click
+
+        #region btnBuscarOrdemServDtGrid Click
+        private void btnBuscarOrdemServDtGrid_Click(object sender, EventArgs e)
+        {
+            this.AbreTelaBuscaOrdemServico();
+        }
+        #endregion btnBuscarOrdemServDtGrid Click
+
+        #region btnBuscaEstoque Click
+        private void btnBuscaEstoque_Click(object sender, EventArgs e)
+        {
+            this.AbreTelaBuscaEstoque();
+        }
+        #endregion btnBuscaEstoque Click
+
+        #region btnAdicionaPeca Click
+        private void btnAdicionaPeca_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion btnAdicionaPeca Click
+
+        #region btnRemovePeca Click
+        private void btnRemovePeca_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion btnRemovePeca Click
+
+        #endregion Eventos
+
+        #region Metodos
+
+        #region Insere
+        /// <summary>
+        /// Insere os dados que estão no model
+        /// </summary>
+        private void Insere()
+        {
+            //Novamente..fazer um codigo bacanudo..^^
+            /*
+            rPecaEstoque regra = new rPecaEstoque();
+
+            try
+            {
+                this.PopulaListaModel();
+                if (this._telaMotor == false)
+                {
+                    this.ValidaDadosNulos();
+
+                    // exclui antes tudo antes de inserir
+                    this.DeletaTudoPorMotor();
+
+                    foreach (mMotorEstoque modelMotorEstoque in this._listaModelMotorEstoque)
+                    {
+                        regra.ValidarInsere(modelMotorEstoque);
+                    }
+                    this.btnLimpa_Click(null, null);
+                    MessageBox.Show("Registro Salvo com Sucesso!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    this.DialogResult = DialogResult.OK;
+                    base.FechaTela(this);
+                }
+            }
+            catch (BUSINESS.Exceptions.MotorEstoque.MotorVazioException)
+            {
+                MessageBox.Show("É Necessário Buscar um Motor", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+                this.btnBuscaMotor.Focus();
+            }
+            catch (BUSINESS.Exceptions.MotorEstoque.EstoqueNaoEscolhidoException)
+            {
+                MessageBox.Show("É Necessário Selecionar um Estoque", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                regra = null;
+            }*/
+        }
+        #endregion Insere
+
+        #region Abre Tela Busca Estoque
+        private void AbreTelaBuscaEstoque()
+        {
+            frmBuscaEstoque telaEstoque = null;
+            try
+            {
+                this._modelEstoque = new mEstoque();
+
+                telaEstoque = new frmBuscaEstoque(_modelEstoque);
+                DialogResult resultado = telaEstoque.ShowDialog();
+                if (resultado == DialogResult.Cancel)
+                {
+                    this._modelEstoque = null;
+                }
+                else
+                {
+                    this.txtEstoque.Text = this._modelEstoque.Dsc_estoque;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                telaEstoque = null;
+            }
+        }
+        #endregion Abre Tela Busca Estoque
+
+        #region Abre Tela Busca OrdemServico
+        private void AbreTelaBuscaOrdemServico()
+        {
+            rOrdemServico regra = new rOrdemServico();
+            frmBuscaCompra telaCompra = null;
+            frmBuscaOrdemServico telaOrdemServ = null;
+
+            DialogResult resultadoOrdemServ;
+            DialogResult resultadoCompra;
+            try
+            {
+                this._modelOrdemServico = new mOrdemServico();
+                this._modelCompra = new mCompra();
+
+                if (rdbBuscaOrdemServ.Checked == true)
+                {
+                    telaOrdemServ = new frmBuscaOrdemServico(this._modelOrdemServico);
+                    resultadoOrdemServ = telaOrdemServ.ShowDialog();
+
+                    if (resultadoOrdemServ == DialogResult.Cancel)
+                    {
+                        this._modelOrdemServico = null;
+                    }
+                    else
+                    {
+                        this.txtBuscaFiltro.Text = "Ordem de Serviço nº " + Convert.ToString(this._modelOrdemServico.IdOrdemServ);
+                    }
+                }
+                else
+                {
+                    telaCompra = new frmBuscaCompra(this._modelCompra);
+                    resultadoCompra = telaCompra.ShowDialog();
+
+                    if (resultadoCompra == DialogResult.Cancel)
+                    {
+                        this._modelCompra = null;
+                    }
+                    else
+                    {
+                        regra.buscaOrdemServicoParamVenda(Convert.ToString(this._modelCompra));
+                        //buscaria o codigo da ordem de servico a partir do codigo da venda..
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                telaCompra = null;
+                //telaOrdemServ = null;
+            }
+        }
+        #endregion Abre Tela Busca OrdemServico
+
+        #region Pega Dados Tela Insumos
+        private mCompra PegaDadosTelaInsumos()
+        {//Fazer algum codigo bacanudo...^^
+            /*mCompra model = new mCompra();
+            rCompra regra = new rCompra();
+            try
+            {
+                model.Dat = Convert.ToDateTime(this.txtDataCompra.Text);
+                model.IdCompra = regra.BuscaIdMaximo();
+                model.Obs = this.txtObs.Text;
+                model.Valor = Convert.ToDouble(this.txtValor.Text);
+                MessageBox.Show("Registro salvo com sucesso!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                model = null;
+            }*/
+            return this._modelCompra;
+            //só p/ num da erro...
+        }
+        #endregion Pega Dados Tela Insumos
+
+        #region Valida Dados Nulos
+        /// <summary>
+        /// Verifica quais dados da tela não podem ser nulos
+        /// </summary>
+        private void ValidaDadosNulos()
+        {
+            //Fazer um codigo bacanudo..^^
+            /*
+            string data = this.txtDataCompra.Text.Replace("/", string.Empty);
+            data = data.Replace(" ", string.Empty);
+            if (string.IsNullOrEmpty(this.txtValor.Text) == true)
+            {
+                throw new BUSINESS.Exceptions.Compra.CompraValorVazioException();
+            }
+            else if (string.IsNullOrEmpty(data) == true)
+            {
+                throw new BUSINESS.Exceptions.Compra.DataCompraVaziaException();
+            }
+            else
+            {
+                BUSINESS.UTIL.Validacoes.ValidaData(this.txtDataCompra.Text);
+            }
+            if (string.IsNullOrEmpty(this.txtQtdItem.Text) == true)
+            {
+                throw new BUSINESS.Exceptions.Compra.CompraQuantidadeVaziaException();
+            }
+            else
+            {
+                int qtd = Convert.ToInt32(this.txtQtdItem.Text);
+                if (qtd < 1)
+                {
+                    throw new BUSINESS.Exceptions.Compra.CompraQuantidadeVaziaException();
+                }
+            }*/
+
+        }
+        #endregion Valida Dados Nulos
+
+        #endregion Metodos
     }
 }
--- CODIGO/TCC/TCC/UI/CADASTRO/frmMotorEstoque.Designer.cs
+++ CODIGO/TCC/TCC/UI/CADASTRO/frmMotorEstoque.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMotorEstoque));
             this.btnRemover = new System.Windows.Forms.Button();
             this.txtQtde = new Controles.MegaTextBox.MegaTextBox(this.components);
@@ -102,39 +102,39 @@
             this.dgEstoques.AllowUserToResizeColumns = false;
             this.dgEstoques.AllowUserToResizeRows = false;
             this.dgEstoques.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgEstoques.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgEstoques.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgEstoques.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgEstoques.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.hIdEstoque,
             this.hNome,
             this.hQuantidade});
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgEstoques.DefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgEstoques.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgEstoques.Location = new System.Drawing.Point(110, 62);
             this.dgEstoques.MultiSelect = false;
             this.dgEstoques.Name = "dgEstoques";
             this.dgEstoques.ReadOnly = true;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgEstoques.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgEstoques.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dgEstoques.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgEstoques.ShowEditingIcon = false;
             this.dgEstoques.Size = new System.Drawing.Size(412, 269);
@@ -186,9 +186,9 @@
             this.lblCdMotor.AutoSize = true;
             this.lblCdMotor.Location = new System.Drawing.Point(109, 21);
             this.lblCdMotor.Name = "lblCdMotor";
-            this.lblCdMotor.Size = new System.Drawing.Size(32, 13);
+            this.lblCdMotor.Size = new System.Drawing.Size(34, 13);
             this.lblCdMotor.TabIndex = 47;
-            this.lblCdMotor.Text = "Peça";
+            this.lblCdMotor.Text = "Motor";
             // 
             // txtMotor
             // 
